deea44b618ca8ce99667001170bb014fe2e17d67
pro-vider <suprodipkundu718@gmail.com>
2021-04-25 11:49:58 +0530

    breaking bad

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../image/logo.jpg';
+const Header = () => {
+    return (
+        <>
+        <header className='center'>
+            <img src={logo} alt=''/>
+        </header>
+        </>
+    );
+};
+
+export default Header;
---
+++ src/components/Spinner.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import spinner from '../image/loader.gif';
+
+const Spinner = () => {
+    return (
+        <img src={spinner} style={{margin:'auto',display:'block'}} alt='Loading'/>
+        
+        
+    )
+}
+
+export default Spinner;
